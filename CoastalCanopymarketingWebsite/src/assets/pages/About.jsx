d49d5884f6247ce77cb4e2294cdfb8749115375a
LawanyaMalavige <lawanyakomudi0408@gmail.com>
2025-02-23 23:35:46 +0530

    Updated Features and About

--- CoastalCanopymarketingWebsite/src/assets/pages/About.jsx
+++ CoastalCanopymarketingWebsite/src/assets/pages/About.jsx
@@ -4,17 +4,15 @@ function About() {
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row items-center">
             <div className="md:w-1/2 mb-10 md:mb-0">
-              <img src="/placeholder.svg" alt="Mangrove conservation team" className="rounded-lg w-full h-auto" />
+              <img src="/Images/Mangrove.jpg" alt="Mangrove conservation team" className="rounded-lg w-full h-auto" />
             </div>
             <div className="md:w-1/2 md:pl-10">
-              <h2 className="text-3xl font-bold mb-6">About MangroveGuard</h2>
-              <p className="mb-4">
-                MangroveGuard is a dedicated team of environmentalists, technologists, and community leaders working
-                together to protect and restore mangrove ecosystems worldwide.
+              <h2 className="text-3xl font-bold mb-6">About CoastalCanopy.org.lk</h2>
+              <p className="mb-4 text-2xl">
+              CoastalCanopy.org.lk is a community-driven platform dedicated to the protection and sustainable management of Sri Lanka’s mangrove ecosystems.
               </p>
-              <p>
-                Our mission is to empower local communities, provide cutting-edge tools for mangrove monitoring, and drive
-                data-informed conservation efforts to ensure the longevity of these crucial coastal habitats.
+              <p className="text-2xl">
+              Our mission is to empower local communities, provide advanced tools for mangrove mapping and monitoring, and promote data-driven conservation efforts to safeguard these vital coastal habitats for future generations.
               </p>
             </div>
           </div>
